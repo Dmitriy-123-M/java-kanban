0ac3c420ece9ff71bb72839bfd5b4f0c79be9f2f
Dimon M <demas123@list.ru>
2025-05-14 23:52:27 +0300

    fix: сделали test на уровне src. FZ-6

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import tasktracker.models.Epic;
--- test/ManagersTest.java
+++ test/ManagersTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import org.junit.jupiter.api.Test;
 import tasktracker.manager.HistoryManager;
 import tasktracker.manager.Managers;
